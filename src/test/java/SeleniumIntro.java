f38ee2376492260f694434c79794c453a4493dba
gourav <sis.gourav17@gmail.com>
2024-04-13 11:35:14 +0530

    Initial commit

---
+++ src/test/java/SeleniumIntro.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class SeleniumIntro {
+    public static void main(String[] args) {
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("Gourav");
+        driver.findElement(By.className("submit")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+        //driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+        //driver.close();
+    }
+}
